62f87bf2a8c401fbf18804d48727c39a67ae7ac3
Ruben Vitt <dev@rubeen.dev>
2020-08-14 16:13:03 +0200

    ⬆️(dependencies): upgrade dependencies

--- src/components/sourcecodeinfo/SourceCodeInfo.component.jsx
+++ src/components/sourcecodeinfo/SourceCodeInfo.component.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 import styles from './SourceCodeInfo.module.css';
-import ReactLogo from './icons8-gitlab.svg';
+import ReactLogo from './github.svg';
 
 function SourceCodeInfo () {
-    const redirect = () => document.location.href = 'https://gitlab.rubeen.dev/rubeen/rubeen.dev-landing-page';
+    const redirect = () => document.location.href = 'https://github.com/rubenvitt/rubeen.dev-landingpage';
 
     return (
         <div onClick={redirect} className={styles.main}>
-            <p><img draggable={"false"} src={ReactLogo} height={'50'} alt={'Gitlab Logo'} /></p>
-            <p>I'm hosted on Gitlab</p>
+            <p><img draggable={"false"} src={ReactLogo} height={'50'} alt={'Github Logo'} /></p>
+            <p>I'm hosted on Github</p>
         </div>
     )
 }
